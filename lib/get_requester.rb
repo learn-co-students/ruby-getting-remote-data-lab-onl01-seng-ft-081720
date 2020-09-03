20174f4a112d7ef65e62ccbd3007fb24d3383632
Eli Cusic <tntitans31@gmail.com>
2020-09-03 08:16:18 -0500

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,32 @@
-# Write your code here
+require 'json'
+require 'net/http'
+require 'open-uri'
+require 'pry'
+
+class GetRequester
+    @@all = []
+
+    attr_reader :url
+
+    def initialize(url)
+        @url = url
+        @@all << @url
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        parsed = JSON.parse(self.get_response_body)
+
+    end
+end
+
+x = GetRequester.new('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+binding.pry
+
+
+# gem "awesome_print", git: "git@github.com:awesome-print/awesome_print.git"
